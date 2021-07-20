835ddece798d515ebbe2acb9e05616078f6efa2a
OPTIMIMCOW <kenallan18@hotmail.com>
2021-07-20 13:40:24 +0100

    In process of generating sample data.

--- Data/SampleData.cs
+++ Data/SampleData.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using ZooManagement.Models.Database;
 using ZooManagement.Data;
+using ZooManagement.Models;
 
 namespace ZooManagement.Data
 {
     public class SampleData
     {
+        private List<ClassificationType> classificationType = new List<ClassificationType> { ClassificationType.Mammal, ClassificationType.Bird, ClassificationType.Fish, ClassificationType.Reptile, 
+            ClassificationType.Insect, ClassificationType.Invertebrate };
+        private List<Sex> sexType = new List<Sex> { Sex.Male, Sex.Female };
+        private int enclosureType = 1;
+        private List<string> speciesType = new List<string> { "Lion", "Hippo", "Gorilla", "Spider", "Penguin", "Octopus", "Zebra", "Bear", "Bat", "Parrot" };
 
-        public static void GenerateData()
-        { }
-            private List<int> slassificationType = new List<int> { 1, 2, 3, 4, 5, 6 };
-            private List<int> sexType = new List<int> { 1, 2 };
-            private int enclosureType = 1;
-            private List<string> speciesType = new List<string> { "Lion", "Hippo", "Gorilla", "Spider", "Penguin", "Octopus", "Zebra", "Bear", "Bat", "Parrot" };
-
-            private static IList<IList<string>> animal = new List<IList<string>>
+        private static IList<IList<string>> animal = new List<IList<string>>
             {
                 new List<string> {"George", DateTime.Now.ToString(), DateTime.Now.ToString() },
                 new List<string> {"Bob", DateTime.Now.ToString(), DateTime.Now.ToString() },
@@ -30,20 +29,39 @@ namespace ZooManagement.Data
                 new List<string> {"Fred", DateTime.Now.ToString(), DateTime.Now.ToString() }
             };
 
-        private static Animal CreateRandomAnimal(int index)
+        public static void GenerateData()
+        {
+            var Animals = Enumerable.Range(0, 10).Select( i => CreateRandomAnimal(i, Generate));
+
+
+        }
+
+        public static Species GenerateSpecies(string type, Classification classification)
+        {
+            return new Species
+            {
+                Type = type,
+                Classification = classification
+            };
+        }
+
+        //Classifications
+
+        //Enclosure
+            
+
+        private static Animal CreateRandomAnimal(int index, List<Species> species)
         {
             return new Animal
             {
                 Name = animal[index][0],
                 DateOfBirth = DateTime.Parse(animal[index][1]),
                 DateAcquired = DateTime.Parse(animal[index][2]),
-                Sex = new Random()
-            }
-            }
-    }
-        
-
-        
+                Sex = (Sex) new Random().Next(1, 2),
+                Species = species[new Random().Next(5)]
+            };
+        }
+    }       
 }
        
 
