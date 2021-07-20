74d1d5907af9ed0191d68e4451c7f07e3b605791
OPTIMIMCOW <kenallan18@hotmail.com>
2021-07-20 15:42:48 +0100

    Added Animal API class. Created AnimalController but not working.

---
+++ Controllers/AnimalController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ZooManagement.Models.Api;
+
+namespace ZooManagement.Controllers
+{
+    [ApiController]
+    [Route("/animal")]
+    public class AnimalController : ControllerBase
+    {
+        private readonly ILogger<AnimalController> _logger;
+
+        public AnimalController(ILogger<AnimalController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<AnimalApi> Get(int id)
+        {
+            // lookup the id in the database
+            // convert the animaldBmodel to API model 
+            //return API model (do we need to json?)
+            return
+        }
+    }
+}
---
+++ Models/Api/AnimalApi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZooManagement.Models.Database;
+
+namespace ZooManagement.Models.Api
+{
+    public class AnimalApi
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateAcquired { get; set; }
+        public string Sex { get; set; }
+        public int EnclosureId { get; set; }
+        public string EnclosureType { get; set; }
+        public string SpeciesName { get; set; }
+
+        public AnimalApi(Animal animalDb)
+        {
+            Id = animalDb.Id;
+            Name = animalDb.Name;
+            Sex = animalDb.Sex.ToString();
+            Age = GetAge(animalDb.DateOfBirth);
+            SpeciesName = animalDb.Species.Type;
+            DateOfBirth = animalDb.DateOfBirth;
+            DateAcquired = animalDb.DateAcquired;
+            EnclosureId = animalDb.Enclosure.Id;
+            EnclosureType = animalDb.Enclosure.Type.ToString();
+        }
+
+        private int GetAge(DateTime birthdate)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - birthdate.Year;
+            if (birthdate.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
+
+
