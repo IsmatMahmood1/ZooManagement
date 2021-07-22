8e35e4b3644026271f5351005615c82364ecab62
OPTIMIMCOW <kenallan18@hotmail.com>
2021-07-22 23:35:07 +0100

    Added zookeeper models and controller and to db context
    Added: DB model, request model, api model, zookeeperController.

---
+++ Controllers/ZooKeeperController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZooManagement.Repositories;
+using ZooManagement.Request;
+using ZooManagement.Models.Api;
+
+namespace ZooManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ZooKeeperController : ControllerBase
+    {
+        private readonly ILogger<AnimalController> _logger;
+        private readonly IZooRepo _zooRepo;
+
+        public ZooKeeperController(ILogger<AnimalController> logger, IZooRepo zooRepo)
+        {
+            _logger = logger;
+            _zooRepo = zooRepo;
+        }
+
+        [HttpGet("{id}")]
+        public ZooKeeperApi Get(int id)
+        {
+            //var animal = _zooRepo.GetAnimalById(id);
+            //return new AnimalApi(animal);
+        }
+
+        [HttpPost("add")]
+        public string Add([FromQuery] AddZooKeeperRequest addZooKeeperRequest)
+        {
+            //var AddSuccess = _zooRepo.AddAnimal(addAnimalRequest);
+            //return AddSuccess;
+        }
+
+    }
+}
---
+++ Models/Api/ZooKeeperApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZooManagement.Models.Api
+{
+    public class ZooKeeperApi
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<int> EnclosureIds { get; set; }
+        public IEnumerable<AnimalApi> Animals { get; set; }
+    }
+}
